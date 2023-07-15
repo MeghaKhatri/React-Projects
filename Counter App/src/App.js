d3bd89ff7106b2f271bf0715af8b0c5e98e2da43
Megha khatri <119837712+MeghaKhatri@users.noreply.github.com>
2023-07-15 10:32:27 +0530

    Update App.js

--- Counter App/src/App.js
+++ Counter App/src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import React, { useState } from 'react'
 
 function App() {
+  
   const [count,setCount] = useState(0);
   let increase =()=> {
     setCount(count + 1)
   }
-  
   let decrease =()=> {
     setCount(count - 1)
   }
+
+  
   return (
     <div className='App'>
       <h1>{count}</h1>
